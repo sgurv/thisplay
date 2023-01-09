420fff8a186cbb6e0ae75850c92fc90d3427057e
Sandeep Guria <sandeepguria@gmail.com>
2023-01-09 18:20:28 +0530

    sntp Time with indian timezone

--- main/main.c
+++ main/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <netdb.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_timer.h"
@@ -11,7 +14,10 @@
 #include "esp_wifi.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
 #include "nvs_flash.h"
+#include "esp_sntp.h"
 #include "lvgl.h"
 //#include "esp_lcd_st7735.h"
 #include "bsp_thisplay.h"
@@ -34,11 +40,13 @@ static lv_disp_t *disp;
 //lv_obj_t * led1;
 
 char clock_str_buff[8];
+char clock_str_footer[32];
 char log_str_buff[128];
 
 static void button_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
 static int button_get_pressed_id(void);
 
+static void initialize_sntp(void);
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -98,6 +106,9 @@ static int button_get_pressed_id(void)
 
 void app_main(void)
 {
+
+    time_t now;
+    struct tm timeinfo;
  
     // ESP_LOGI(TAG, "Test GPIO LED!");
     // gpio_reset_pin(BLINK_TEST_GPIO);
@@ -123,6 +134,20 @@ void app_main(void)
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+        /**
+     * NTP server address could be aquired via DHCP,
+     * see following menuconfig options:
+     * 'LWIP_DHCP_GET_NTP_SRV' - enable STNP over DHCP
+     * 'LWIP_SNTP_DEBUG' - enable debugging messages
+     *
+     * NOTE: This call should be made BEFORE esp aquires IP address from DHCP,
+     * otherwise NTP option would be rejected by default.
+     */
+#ifdef LWIP_DHCP_GET_NTP_SRV
+    sntp_servermode_dhcp(1);      // accept NTP offers from DHCP server, if any
+#endif
+
     esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -149,6 +174,17 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
+    //Time
+
+    // init sntp
+    initialize_sntp();
+
+    time(&now); // Get system time
+    // Set timezone to Indian Standard Time
+    setenv("TZ", "IST-5:30", 1); // Set PISIX environment variable 'timezone' // minus(-) according to POSIX
+    tzset(); //update C library runtime data for the new timezone.
+
+    //End Time stuff
     //--Display
     disp = bsp_display_start();
 
@@ -227,8 +263,16 @@ void app_main(void)
                 }
                 minute = 0;
             }
-            snprintf(clock_str_buff,8,"%02d:%02d",hour,minute);
+
+            time(&now);
+            localtime_r(&now, &timeinfo);
+
+            //snprintf(clock_str_buff,8,"%02d:%02d",hour,minute);
+            snprintf(clock_str_buff,8,"%02d:%02d",timeinfo.tm_hour,timeinfo.tm_min);
             lv_label_set_text(ui_Screen1_Label1, clock_str_buff);
+
+            strftime(clock_str_footer, sizeof(clock_str_footer), "%a,%d %b %Y", &timeinfo);
+            lv_label_set_text(ui_Screen1_Label3, clock_str_footer);
 
             lv_textarea_set_text(ui_Screen5_TextArea1, log_str_buff);
 
@@ -237,4 +281,53 @@ void app_main(void)
         bsp_display_unlock();
     }
 
+} //End app_main()
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+
+/*
+ * If 'NTP over DHCP' is enabled, we set dynamic pool address
+ * as a 'secondary' server. It will act as a fallback server in case that address
+ * provided via NTP over DHCP is not accessible
+ */
+#if LWIP_DHCP_GET_NTP_SRV && SNTP_MAX_SERVERS > 1
+    sntp_setservername(1, "pool.ntp.org");
+
+#if LWIP_IPV6 && SNTP_MAX_SERVERS > 2          // statically assigned IPv6 address is also possible
+    ip_addr_t ip6;
+    if (ipaddr_aton("2a01:3f7::1", &ip6)) {    // ipv6 ntp source "ntp.netnod.se"
+        sntp_setserver(2, &ip6);
+    }
+#endif  /* LWIP_IPV6 */
+
+#else   /* LWIP_DHCP_GET_NTP_SRV && (SNTP_MAX_SERVERS > 1) */
+    // otherwise, use DNS address from a pool
+    //sntp_setservername(0, CONFIG_SNTP_TIME_SERVER);
+
+    sntp_setservername(1, "pool.ntp.org");     // set the secondary NTP server (will be used only if SNTP_MAX_SERVERS > 1)
+#endif
+
+    //sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+#endif
+    sntp_init();
+
+    ESP_LOGI(TAG, "List of configured NTP servers:");
+
+    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i){
+        if (sntp_getservername(i)){
+            ESP_LOGI(TAG, "server %d: %s", i, sntp_getservername(i));
+        } else {
+            // we have either IPv4 or IPv6 address, let's print it
+            char buff[INET6_ADDRSTRLEN];
+            ip_addr_t const *ip = sntp_getserver(i);
+            if (ipaddr_ntoa_r(ip, buff, INET6_ADDRSTRLEN) != NULL)
+                ESP_LOGI(TAG, "server %d: %s", i, buff);
+        }
+    }
 }
+
