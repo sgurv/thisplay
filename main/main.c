465d007e0d69dbed6f2c185499ed3818db0accb0
Sandeep Guria <sandeepguria@gmail.com>
2023-10-03 12:03:47 +0530

    updated components

--- main/main.c
+++ main/main.c
@@ -152,7 +152,7 @@ void app_main(void)
      * otherwise NTP option would be rejected by default.
      */
 #ifdef LWIP_DHCP_GET_NTP_SRV
-    sntp_servermode_dhcp(1);      // accept NTP offers from DHCP server, if any
+    esp_sntp_servermode_dhcp(1);      // accept NTP offers from DHCP server, if any
 #endif
 
     esp_netif_create_default_wifi_sta();
@@ -285,7 +285,7 @@ void app_main(void)
 static void initialize_sntp(void)
 {
     ESP_LOGI(TAG, "Initializing SNTP");
-    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
 
 /*
  * If 'NTP over DHCP' is enabled, we set dynamic pool address
@@ -293,7 +293,7 @@ static void initialize_sntp(void)
  * provided via NTP over DHCP is not accessible
  */
 #if LWIP_DHCP_GET_NTP_SRV && SNTP_MAX_SERVERS > 1
-    sntp_setservername(1, "pool.ntp.org");
+    esp_sntp_setservername(1, "pool.ntp.org");
 
 #if LWIP_IPV6 && SNTP_MAX_SERVERS > 2          // statically assigned IPv6 address is also possible
     ip_addr_t ip6;
@@ -306,24 +306,24 @@ static void initialize_sntp(void)
     // otherwise, use DNS address from a pool
     //sntp_setservername(0, CONFIG_SNTP_TIME_SERVER);
 
-    sntp_setservername(1, "pool.ntp.org");     // set the secondary NTP server (will be used only if SNTP_MAX_SERVERS > 1)
+    esp_sntp_setservername(1, "pool.ntp.org");     // set the secondary NTP server (will be used only if SNTP_MAX_SERVERS > 1)
 #endif
 
     //sntp_set_time_sync_notification_cb(time_sync_notification_cb);
 #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
     sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
 #endif
-    sntp_init();
+    esp_sntp_init();
 
     ESP_LOGI(TAG, "List of configured NTP servers:");
 
     for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i){
-        if (sntp_getservername(i)){
-            ESP_LOGI(TAG, "server %d: %s", i, sntp_getservername(i));
+        if (esp_sntp_getservername(i)){
+            ESP_LOGI(TAG, "server %d: %s", i, esp_sntp_getservername(i));
         } else {
             // we have either IPv4 or IPv6 address, let's print it
             char buff[INET6_ADDRSTRLEN];
-            ip_addr_t const *ip = sntp_getserver(i);
+            ip_addr_t const *ip = esp_sntp_getserver(i);
             if (ipaddr_ntoa_r(ip, buff, INET6_ADDRSTRLEN) != NULL)
                 ESP_LOGI(TAG, "server %d: %s", i, buff);
         }
